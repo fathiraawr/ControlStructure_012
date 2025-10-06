72fdb26786803480bec2a5c3052922413272a10d
fathiraawr <fathircraft185@gmail.com>
2025-10-06 11:00:43 +0700

    Fibinacci

--- 3.py
+++ 3.py
@@ -1,1 +1,9 @@
-print("h")
+
+n = int(input("Masukkan nilai maksimum Fibonacci: "))
+
+a, b = 0, 1
+
+print("Deret Fibonacci hingga", n, ":")
+while a <= n:
+    print(a, end=" ")
+    a, b = b, a + b
